84f5ce122f58aa054e4f5917bf9986c82ed50925
Arun Venugopal <arun.naagarajan@gmail.com>
2017-09-08 16:00:36 +0530

    Updated Puchi
    Added the code to filter files based on extension and search for the keywords

--- Puchi.java
+++ Puchi.java
@@ -3,59 +3,119 @@ package com.system.puchisoftware;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 public class Search 
 {
- 
+	private static ArrayList<String> recurselistOfpath = new ArrayList<String>();
+
 	public static void main(String args[])
 	{
-		
-       try
-       {
-    	Map<Integer,String> mapUtil = new HashMap<Integer,String>();
-    	FileReader filereaderObject = new FileReader(new File("D:\\MYHEC273848D\\Arun\\HTMLReport\\src\\createReport\\create.java"));
-		BufferedReader bw = new BufferedReader(filereaderObject);
-		String line = null;
-		String searchKeyword = "java";
-	
-		int lineCounter=0;
-		while((line = bw.readLine())!= null)
+		try
 		{
-		 	lineCounter++;
-		 	if(line.contains(searchKeyword))
-		 	{
-		 		mapUtil.put(lineCounter,line);
-		 	}
-		}
-		System.out.println("Total line in the document "+ lineCounter);
-		
-		Set<Integer> setObject = mapUtil.keySet();
-		
-		if(!setObject.isEmpty())
-		{
-			for (Integer integer : setObject) 
+			String filePath = null;
+			String searchKeyword = null;
+			String fileExtensions = null;
+			FileReader filereaderObject = null;
+			BufferedReader bw = null;
+			Map<Integer,String> mapUtil = new HashMap<Integer,String>();
+			int lineCounter=0;
+	    	
+			for (String string : args) 
 			{
-				System.out.println("String " + searchKeyword + " found in line number "+ integer +" line matching "+ mapUtil.get(integer));
+				System.out.println(string);
+			}
+			
+			if(args.length < 0)
+	    	{
+	    		System.out.println("Provide all the arguments");
+	    	}
+	    	else
+	    	{
+	    		filePath = args[0].split("=")[1];
+	    		searchKeyword = args[1].split("=")[1];
+	    		fileExtensions = args[2].split("=")[1];
+	    	}
+
+			getRecursePath(filePath);
+			String[] seperateExtensions = fileExtensions.split("#~");
+			for (String sepPath : recurselistOfpath) 
+			{
+
+				File fileObject = new File(sepPath);
+
+				if(fileObject.isFile())
+				{
+					String extension = fileObject.getAbsoluteFile().getName().substring(fileObject.getAbsoluteFile().getName().lastIndexOf("."),fileObject.getAbsoluteFile().getName().length());
+
+					for (String string : seperateExtensions) 
+					{
+						if(extension.equalsIgnoreCase(string))
+						{
+							filereaderObject = new FileReader(fileObject);
+							bw = new BufferedReader(filereaderObject);
+							String line = null;
+							while((line = bw.readLine())!= null)
+							{
+								lineCounter++;
+								if(line.contains(searchKeyword))
+								{
+									mapUtil.put(lineCounter,line);
+								}
+							}
+							System.out.println("****************************************************************");
+							System.out.println("File Path " + sepPath);
+							System.out.println("Total line in the document "+ lineCounter);
+
+							Set<Integer> setObject = mapUtil.keySet();
+
+							if(!setObject.isEmpty())
+							{
+								for (Integer integer : setObject) 
+								{
+									System.out.println("String " + searchKeyword + " found in line number "+ integer +" line matching "+ mapUtil.get(integer));
+								}
+							}
+							else
+							{
+								System.out.println("No Search Keyword found");
+							}
+							System.out.println("End of search in " + sepPath);
+							System.out.println("****************************************************************");
+						}
+					}
+				}
+
+			}
+
+
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public static void getRecursePath(String filePath)
+	{
+		File fileDirectoryAndFiles = new File(filePath);
+		File[] fileArray = fileDirectoryAndFiles.listFiles();
+
+		for (File file2 : fileArray) 
+		{
+			if(file2.isDirectory())
+			{
+				getRecursePath(file2.getAbsolutePath());	
+			}
+			else
+			{
+				recurselistOfpath.add(file2.getAbsolutePath());
 			}
 		}
-		else
-		{
-			System.out.println("No Search Keyword found");
-		}
-		
-       }
-       catch(Exception e)
-       {
-    	    //Exception here
-       }
 	}
 
-	
-	
-	
-	
+
+
 }
